a900d61e2947c1053f2d89b48e0e561661d36338
mel-10 <81254766+mel-10@users.noreply.github.com>
2024-04-21 17:43:28 +0530

    Update cnn.py

--- cnn.py
+++ cnn.py
@@ -36,19 +36,19 @@ def create_train_data():
         shapes.add(img.shape)
         
         training_data.append([np.array(img), np.array(label)])
-        training_data_types = [type(item) for item in training_data]
-        unique_training_data_types = np.unique(training_data_types)
+    
+    shuffle(training_data)
+    
+    # Check the data types of the elements in training_data
+    training_data_types = [type(item) for item in training_data]
+    unique_training_data_types = np.unique(training_data_types)
+    if len(unique_training_data_types) > 1:
+        print("Data types are different at indices:")
+        for i, item in enumerate(training_data_types):
+            print(f"Index {i}: {item}")
+    else:
+        print("Data types are consistent throughout the array.")
 
-        if len(unique_training_data_types) > 1:
-            print("Data types are different at indices:")
-            for i, item in enumerate(training_data_types):
-                print(f"Index {i}: {item}")
-        else:
-            print("Data types are consistent throughout the array.")
-      
-    shuffle(training_data)  
-    #np.save('train_data.npy', np.array(training_data))  # Convert list to numpy array before saving
-    m= np.array(training_data)
     return training_data
 
     
